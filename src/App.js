b6fe2e438b9ee5eacf476ce545d60d72e09f00be
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-13 15:45:53 +0300

    Added Markup for Programs section and it's CSS styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import "./App.css";
+import Programs from "./components/Programs/Programs";
 
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs/>
     </div>
   );
 }
---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,31 @@
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
+import "./Programs.css";
+
+const Programs = () => {
+  return (
+    <div className="programs" id="programs">
+      {/* header */}
+      <div className="programs-header">
+        <span className="stroke-text">Explore our</span>
+        <span>Programs</span>
+        <span className="stroke-text">to shape you</span>
+      </div>
+      <div className="program-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="arrow" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Programs;
