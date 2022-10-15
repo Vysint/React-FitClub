1a4dc0599d8b18a9d2783344abdd240c641ecb86
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-15 22:42:38 +0300

    Add Navbar Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import Logo from "../../assets/logo.png";
 import Bars from "../../assets/bars.png";
-import {Link} from 'react-scroll';
+import { Link } from "react-scroll";
 import "./Header.css";
 import { useState } from "react";
 
@@ -17,8 +17,7 @@ const Header = () => {
             padding: "0.5rem",
             borderRadius: "5px",
           }}
-
-          onClick = {() => setMenuOpened(true)}
+          onClick={() => setMenuOpened(true)}
         >
           <img
             src={Bars}
@@ -28,11 +27,56 @@ const Header = () => {
         </div>
       ) : (
         <ul className="header-menu">
-          <li onClick = {() => setMenuOpened(false)}>Home</li>
-          <li onClick = {() => setMenuOpened(false)}>Programs</li>
-          <li onClick = {() => setMenuOpened(false)}>Why Us</li>
-          <li onClick = {() => setMenuOpened(false)}>Plans</li>
-          <li onClick = {() => setMenuOpened(false)}>Testimonials</li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="home"
+              span={true}
+              smooth={true}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="programs"
+              span={true}
+              smooth={true}
+            >
+              Programs
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="reasons"
+              span={true}
+              smooth={true}
+            >
+              Why Us
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="plans"
+              span={true}
+              smooth={true}
+            >
+              Plans
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              to="testimonials"
+              span={true}
+              smooth={true}
+            >
+              Testimonials
+            </Link>
+          </li>
         </ul>
       )}
     </div>
