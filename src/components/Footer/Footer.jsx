fef5da96c5644039facf5667cf27d2c5cb630d72
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-15 14:03:53 +0300

    Added Footer Section Markup and it's CSS styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import Logo from "../../assets/logo.png";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <hr />
+      <div className="footer">
+        <div className="social-links">
+          <a
+            href="https://github.com"
+            target="__blank"
+            rel="noreferrer noopener"
+          >
+            <img src={Github} alt="Github" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="__blank"
+            rel="noreferrer noopener"
+          >
+            <img src={Instagram} alt="Instagram" />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="__blank"
+            rel="noreferrer noopener"
+          >
+            <img src={LinkedIn} alt="Linkedin" />
+          </a>
+        </div>
+        <div className="logo-f">
+          <img src={Logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
